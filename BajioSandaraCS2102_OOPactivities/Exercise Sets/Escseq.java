f24e6026b6740f969c8c84a08b9f89dc992a67f3
SandaraBajio <163809575+SandaraBajio@users.noreply.github.com>
2024-08-30 14:45:49 +0800

    Delete BajioSandaraCS2102_OOPactivities/Exercise Sets/Escseq.java

--- BajioSandaraCS2102_OOPactivities/Exercise Sets/Escseq.java
+++
@@ -1,8 +0,0 @@
-class Escseq{
-    public static void main(String[]args){
-        System.out.println("7");
-        System.out.println("\t14");
-        System.out.println("21");
-        System.out.println("\t28");
-        System.out.println("35");}
-}
